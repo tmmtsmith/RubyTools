3c95fceac1509ecab8305c437ca30cc1cecdb08e
Tim <tmmtsmith@gmail.com>
2013-02-27 06:13:34 -0600

    Contents, line by line

--- RubyETL/lib/ruby_etl.rb
+++ RubyETL/lib/ruby_etl.rb
@@ -1,1 +1,11 @@
-puts "RubyETL Initialized!"
+puts "CSV Loader"
+
+# Reads the file line-by-line
+lines = File.readlines "CSVfiles\\example.csv"
+lines.each do |line|
+	puts line
+end
+
+# Note: Reads the file's entire contents
+# contents = File.read "CSVfiles\\example.csv"
+# puts contents
