b54195c79f0ce8c0f85ac4c212bf9efec2ec469b
Tim <tmmtsmith@gmail.com>
2013-02-27 06:31:58 -0600

    Skipping rows

--- RubyETL/lib/ruby_etl.rb
+++ RubyETL/lib/ruby_etl.rb
@@ -2,13 +2,17 @@ puts "CSV Loader"
 
 # Reads the file line-by-line
 lines = File.readlines "CSVfiles\\example.csv"
-lines.each do |line|
+# This declares a "beginning"
+lines.each_with_index do |line,index|
+	# If the row is 0 skip it and continue (see last line before end)
+	next if index == 0
 	# We declared the object line, and specified another object column from the object line
 	# Specifies the delimiter for the columns
 	columns = line.split(",")
 	# Grabs the third array value, in this case First Name
 	first_name = columns[2]
 	puts first_name
+	index
 end
 
 # Note: Reads the file's entire contents
