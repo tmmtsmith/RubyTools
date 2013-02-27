0bd7aa1d0a0845494450343e8a374a1fb4fde799
Tim <tmmtsmith@gmail.com>
2013-02-27 06:20:51 -0600

    Notes and specific data

--- RubyETL/lib/ruby_etl.rb
+++ RubyETL/lib/ruby_etl.rb
@@ -3,7 +3,12 @@ puts "CSV Loader"
 # Reads the file line-by-line
 lines = File.readlines "CSVfiles\\example.csv"
 lines.each do |line|
-	puts line
+	# We declared the object line, and specified another object column from the object line
+	# Specifies the delimiter for the columns
+	columns = line.split(",")
+	# Grabs the third array value, in this case First Name
+	first_name = columns[2]
+	puts first_name
 end
 
 # Note: Reads the file's entire contents
