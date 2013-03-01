32ec8d58784ee61f2efdf723c8128e545729cb10
Tim <tmmtsmith@gmail.com>
2013-03-01 07:53:52 -0600

    Another example
    Helps compare how symbols seek data

--- RubyETL/lib/classetl.rb
+++ RubyETL/lib/classetl.rb
@@ -2,6 +2,8 @@ require "csv"
 
 contents = CSV.open "CSVfiles\\example.csv", headers: true, header_converters: :symbol
 contents.each do |row|
+	# The symbol in the brackets pulls data determined in the heading row
 	first_name = row[:first_name]
-	puts first_name
+	zip = row[:zipcode]
+	puts "#{first_name} #{zip}"
 end
