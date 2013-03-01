52d44116e907e6cb86fb65ebca8feefa4f7d333e
Tim <tmmtsmith@gmail.com>
2013-03-01 07:54:57 -0600

    Testing symbols

--- RubyETL/lib/classetl.rb
+++ RubyETL/lib/classetl.rb
@@ -2,7 +2,8 @@ require "csv"
 
 contents = CSV.open "CSVfiles\\example.csv", headers: true, header_converters: :symbol
 contents.each do |row|
-	# The symbol in the brackets pulls data determined in the heading row
+	# The symbol in the brackets pulls data determined in the heading row; therefore data must have 
+	# a match with the the symbol
 	first_name = row[:first_name]
 	zip = row[:zipcode]
 	puts "#{first_name} #{zip}"
