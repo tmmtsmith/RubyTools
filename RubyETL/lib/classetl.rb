df0b17e0d435d4cb8025dbfe04d808f77471bfc5
Tim <tmmtsmith@gmail.com>
2013-03-01 07:47:43 -0600

    Rows as symbols

--- RubyETL/lib/classetl.rb
+++ RubyETL/lib/classetl.rb
@@ -1,7 +1,7 @@
 require "csv"
 
-contents = CSV.open "CSVfiles\\example.csv", headers: true
+contents = CSV.open "CSVfiles\\example.csv", headers: true, header_converters: :symbol
 contents.each do |row|
-	first_name = row[2]
+	first_name = row[:first_name]
 	puts first_name
 end
