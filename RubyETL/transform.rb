e814bdab88ae65accc3eaf78fb76ba056846a9ed
Tim <tmmtsmith@gmail.com>
2013-03-01 10:40:20 -0600

    Manipulating bad zip codes

--- RubyETL/transform.rb
+++ RubyETL/transform.rb
@@ -3,8 +3,18 @@
 require "csv"
 
 contents = CSV.open "CSVfiles\\example.csv", headers: true, header_converters: :symbol
+
 contents.each do |row|
 	first_name = row[:first_name]
 	zip = row[:zipcode]
+	
+	if zip.length < 5
+		zip = zip.rjust 5, "0"
+	elsif zip.length > 5
+		zip = zip[0..4]
+	elsif zip.length == nil
+		zip = "00000"
+	end
+	
 	puts "#{first_name} #{zip}"
 end
