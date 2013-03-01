6fb1f939f0f78ec426aad2cb218f2ffd06c8295a
Tim <tmmtsmith@gmail.com>
2013-03-01 11:03:00 -0600

    Zip and phone now methods

--- RubyETL/transform.rb
+++ RubyETL/transform.rb
@@ -2,29 +2,37 @@
 
 require "csv"
 
+def clean_zip(zip)
+	if zip.nil?
+		zip = "N/A"
+	elsif zip.length < 5
+		zip = zip.rjust(5,"0")
+	elsif zip.length > 5
+		zip = zip[0..4]
+	else
+		zip
+	end
+end
+
+def clean_phone(phone)
+	if phone.nil?
+		phone = "N/A"
+	else
+		phone
+	end
+end
+
 contents = CSV.open "CSVfiles\\example.csv", headers: true, header_converters: :symbol
 
 contents.each do |row|
 	first_name = row[:first_name]
-	phone = row[:homephone]
-	zip = row[:zipcode]
-	
-	if phone.nil?
-		phone = "N/A"
-	end
+	phone = clean_phone(row[:homephone])
+	zip = clean_zip(row[:zipcode])
 	
 	#phone["("] = ""
 	#phone[")"] = ""
 	#phone["-"] = ""
 	#phone[" "] = ""
-	
-	if zip.nil?
-		zip = "N/A"
-	elsif zip.length < 5
-		zip = zip.rjust 5, "0"
-	elsif zip.length > 5
-		zip = zip[0..4]
-	end
 	
 	puts "#{first_name} #{phone} #{zip}"
 end
