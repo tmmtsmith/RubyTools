d2a09ce2fc149a478a5e9e00ac841a3364ed6116
Tim <tmmtsmith@gmail.com>
2013-03-01 07:59:13 -0600

    Load finished, manipulate added

--- RubyETL/lib/transform.rb
+++ RubyETL/lib/transform.rb
@@ -1,11 +1,9 @@
-# Simple and basic load file
+# Opens, manipulates and loads
 
 require "csv"
 
 contents = CSV.open "CSVfiles\\example.csv", headers: true, header_converters: :symbol
 contents.each do |row|
-	# The symbol in the brackets pulls data determined in the heading row; therefore data must have 
-	# a match with the the symbol
 	first_name = row[:first_name]
 	zip = row[:zipcode]
 	puts "#{first_name} #{zip}"
