bf878a6eaecb44ce6b000feaaa9e4491635d0283
Tim <tmmtsmith@gmail.com>
2013-11-10 06:27:11 -0600

    Interpreting server responses

---
+++ Python/serverresp.py
@@ -0,0 +1,14 @@
+# Note:
+
+# 2XX means successful response.
+# 3XX means failure because another process required first.
+# 4XX means you made a mistake (think 404).
+# 5XX server failed
+
+# Example
+import requests
+res = requests.get('http://www.[WEBSITE].com/')
+print res.status_code
+print res.headers
+
+# GET (select), POST (insert), PUT (upadte), DELETE (delete)
