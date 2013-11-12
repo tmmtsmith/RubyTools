543afa60cd6609fc5c9d55462c60ae2eea479e37
Tim <tmmtsmith@gmail.com>
2013-11-12 05:49:04 -0600

    Update strings.py

--- Python/strings.py
+++ Python/strings.py
@@ -21,3 +21,24 @@ if len(original) > 0 and original.isalpha():
         print res
 else:
     print 'Invalid entry.'
+
+
+
+## String & Function example
+
+def get_calories (c,p,f):
+    c_cal = 4 * c
+    p_cal = 4 * p
+    f_cal = 9 * p
+    x = (c_cal + p_cal + f_cal)
+    per = macro_percent(c_cal,p_cal,f_cal,x)
+    return "Calories for today: %s , breakdown by percent in carbs, protein and fat: %s" % (x,per)
+    
+def macro_percent(c,p,f,t):
+    cm = (c/float(t))
+    pm = (p/float(t))
+    fm = (f/float(t))
+    return cm, pm, fm
+    
+y = get_calories(200,200,100)
+print y
