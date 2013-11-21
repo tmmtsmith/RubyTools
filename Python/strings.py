5ed9527d412dfa820c1f1162473655bcedb4be63
Tim <tmmtsmith@gmail.com>
2013-11-21 05:23:36 -0600

    Update strings.py

--- Python/strings.py
+++ Python/strings.py
@@ -95,3 +95,52 @@ def pb(b):
 print pb(s)
 # 12 columns, 9 rows
 print pb(t)
+
+
+# "Where's the Tiger?" game example from CodeAc:
+
+from random import randint
+
+board = []
+
+for x in range(5):
+    board.append(["O"] * 5)
+
+def print_board(board):
+    for row in board:
+        print " ".join(row)
+
+print "Where's the tiger?"
+print_board(board)
+
+def random_row(board):
+    return randint(0, len(board) - 1)
+
+def random_col(board):
+    return randint(0, len(board[0]) - 1)
+
+tig_row = random_row(board)
+tig_col = random_col(board)
+# Debugging
+#print tig_row
+#print tig_col
+
+for turn in range(4):
+    guess_row = int(raw_input("Guess Row:"))
+    guess_col = int(raw_input("Guess Col:"))
+    
+    if guess_row == tig_row and guess_col == tig_col:
+        print "There he is!"
+        break
+    else:
+        if turn == 3:
+            print "Game Over"
+        elif (guess_row < 0 or guess_row > 4) or (guess_col < 0 or guess_col > 4):
+            print "He's not in the ground."
+        elif(board[guess_row][guess_col] == "X"):
+            print "You already tried to find him there."
+        else:
+            print "Miss!"
+            board[guess_row][guess_col] = "X"
+        print_board(board)
+    print str(turn + 1)
