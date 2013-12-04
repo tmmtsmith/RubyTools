fbe8adbcfa2451616c02efc4cadf043d0cf24c45
Tim <tmmtsmith@gmail.com>
2013-12-04 05:07:57 -0600

    Create modules.rb

---
+++ modules.rb
@@ -0,0 +1,18 @@
+
+module SqlConnection
+
+    ## Note: this is not an actual connection, but testing modules and references to them
+    UN = "UU"
+    PA = "PP"
+    
+    ## syntax self (referring to current module) then method name
+    ## other way: def SqlConnection.connect
+    def self.connect(db,sv)
+        scon = "Connection string with " + db + " and " + sv
+        scon += " p:" + PA + " u:" + UN
+        return scon
+    end
+
+end
+
+puts SqlConnection::connect("OurDB","OurServer")
